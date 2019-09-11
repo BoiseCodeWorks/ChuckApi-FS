962718166f97a0c3c6eec39896b20c04896438fe
Kenny Henbest <lekenbest@gmail.com>
2019-09-10 18:07:54 -0600

    Multiple Instances

---
+++ src/components/Joke.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="Joke container">
+        <div class="row justify-content-end align-items-center">
+            <div class="col-6">
+                <h1 class="vueGreen"><img alt="Vue logo"
+                        src="https://images02.military.com/sites/default/files/styles/full/public/media/veteran-jobs/content-images/2016/03/chucknorris.jpg?itok=_J3M4O-S">ery
+                    Chuck Jokes</h1>
+                <p>Laughs: {{currentLaughs}}</p>
+            </div>
+        </div>
+        <div class="row justify-content-start align-items-center mt-5">
+            <div class="card vueGreen col-6">
+                <div class="card-body">
+                    <p class="card-text">{{joke.value}}</p>
+                    <p class="card-text">{{msg}}</p>
+                    <button @click="newJoke" class="btn btn-success">Get New Joke</button>
+                    <button @click="laugh" class="btn btn-success">Haha</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Joke",
+        props: ["msg"],
+        mounted() {
+            this.newJoke()
+        },
+        data() {
+            return {
+                currentLaughs: 0
+            }
+        },
+        computed: {
+            joke() {
+                return this.$store.state.joke
+            },
+            laughs() {
+                return this.$store.state.laughCount
+            }
+        },
+        methods: {
+            newJoke() {
+                this.$store.dispatch('getJoke')
+            },
+            laugh() {
+                this.currentLaughs += 1
+            }
+        },
+        components: {}
+    }
+</script>
+<style>
+</style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import Axios from 'axios'
 Vue.use(Vuex)
 
+const api = Axios.create({
+  baseURL: "https://api.chucknorris.io/jokes/random",
+  timeout: 3000
+})
 export default new Vuex.Store({
   state: {
-
+    joke: {},
+    laughCount: 0
   },
   mutations: {
-
+    setJoke(state, data) {
+      state.joke = data
+    }
   },
   actions: {
+    async getJoke({ commit, dispatch }) {
+      try {
+        let res = await api.get('')
+        commit("setJoke", res.data)
 
+      } catch (error) {
+        console.error(error)
+      }
+    }
   }
 })
